2f9cfdf49f4a89bc5729512eff94b9b7ad4b5ac8
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2015-11-03 21:50:28 +0100

    very good moving behaviour

--- src/dkck/ItemsOperations.java
+++ src/dkck/ItemsOperations.java
@@ -52,7 +52,7 @@ public class ItemsOperations {
 		itemsArray.add((Item) new Bomb(16, 8, 5, 30));
 		itemsArray.add((Item) new Bomb(7, 20, 5, 30));
 		itemsArray.add((Item) new Sapper(2, 7, 2, 50));
-		itemsArray.add((Item) new Sapper(8, 9, 2, 100));
+		itemsArray.add((Item) new Sapper(8, 9, 2, 70));
 
 		for (int i = 0; i < itemsArray.size(); i++) {
 			Item tempItem = itemsArray.get(i);
