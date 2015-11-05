bec051e2222e45ff0ae127a53a1aad47f3a351bf
piotrowiczmateusz <piotrowiczmateusz@onet.pl>
2015-11-05 11:25:30 +0100

    chagnes

--- src/dkck/Interpreter.java
+++ src/dkck/Interpreter.java
@@ -9,28 +9,41 @@ public class Interpreter {
 	}
 	
 	public void interpret(String input) throws InterruptedException {
-		if(input.matches("(.*)go(.*)|(.*)idŸ(.*)|(.*)pójdŸ(.*)")) {
+		
+		/* Przemieszczanie siê */
+		
+		if(input.matches("|(.*)idŸ(.*)|(.*)pójdŸ(.*)")) {
 			
 			int x = ((Sapper) MainWindow.itemsCollection.getItemsArray().get(3)).getPositionX();
 			int y = ((Sapper) MainWindow.itemsCollection.getItemsArray().get(3)).getPositionY();
 			
-			if(input.matches("(.*)pó³noc(.*)|(.*)north(.*)")) {
-				x = 0;
-				((Sapper) MainWindow.itemsCollection.getItemsArray().get(3)).go(x, y);
-			}
-			if(input.matches("(.*)zachód(.*)|(.*)lewo(.*)")) {
-				y = 0;
-				((Sapper) MainWindow.itemsCollection.getItemsArray().get(3)).go(x, y);
-			}
-			if(input.matches("(.*)po³udnie(.*)|(.*)south(.*)")) {
-				x = 49;
-				((Sapper) MainWindow.itemsCollection.getItemsArray().get(3)).go(x, y);
-			}
-			if(input.matches("(.*)wschód(.*)|(.*)prawo(.*)")) {
-				y = 49;
-				((Sapper) MainWindow.itemsCollection.getItemsArray().get(3)).go(x, y);
+			if(input.matches("(.*)[0-9](.*)")) {
+				for(int i = 0; i < input.length()-1; i++) {
+					if(Character.isDigit(input.charAt(i))) {
+						x = input.charAt(i) - 48;			
+						
+						System.out.println(x);
+					}
+		
+				}			
 			}
 			
+		
+			if(input.matches("(.*)pó³noc(.*)|(.*)pó³nocny(.*)")) {
+				x = 0;		
+			}
+			else if(input.matches("(.*)po³udnie(.*)|(.*)po³udniowy(.*)")) {
+				x = 49;			
+			}
+			if(input.matches("(.*)zachód(.*)|(.*)zachodni(.*)|(.*)lewo(.*)|(.*)lewy(.*)")) {
+				y = 0;			
+			}		
+			else if(input.matches("(.*)wschód(.*)|(.*)wschodni(.*)|(.*)prawo(.*)|(.*)prawy(.*)")) {
+				y = 49;				
+			}
+			
+			
+			((Sapper) MainWindow.itemsCollection.getItemsArray().get(3)).go(x, y);
 			
 		}
 		else {
